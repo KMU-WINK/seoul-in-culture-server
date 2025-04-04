174666499b3f31edda14274f79a327d95f2b4a69
didskfo <didskfo2003@gmail.com>
2025-04-05 00:15:16 +0900

    feat(user): Swagger 코드 추가

--- src/main/java/com/github/kmu_wink/seoul_in_culture/domain/user/controller/UserController.java
+++ src/main/java/com/github/kmu_wink/seoul_in_culture/domain/user/controller/UserController.java
@@ -6,6 +6,8 @@ import com.github.kmu_wink.seoul_in_culture.domain.user.dto.UserEditRequest;
 import com.github.kmu_wink.seoul_in_culture.domain.user.dto.UserEditResponse;
 import com.github.kmu_wink.seoul_in_culture.domain.user.schema.User;
 import com.github.kmu_wink.seoul_in_culture.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -13,11 +15,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
+@Tag(name = "User-Controller", description = "유저-관련-API")
 public class UserController {
 
     private final UserService userService;
 
     @PutMapping
+    @Operation(summary = "프로필 수정", description = "유저의 프로필 수정합니다.")
     public ApiResponse<UserEditResponse> editUser(
             @RequestBody UserEditRequest request,
             @AuthenticationPrincipal User user
@@ -26,6 +30,7 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(summary = "내 상세 페이지 조회", description = "내 상세 페이지를 조회합니다.")
     public ApiResponse<MyDetailResponse> getMyDetail(
             @AuthenticationPrincipal User user
     ) {
--- src/main/java/com/github/kmu_wink/seoul_in_culture/domain/user/dto/UserEditRequest.java
+++ src/main/java/com/github/kmu_wink/seoul_in_culture/domain/user/dto/UserEditRequest.java
@@ -1,10 +1,21 @@
 package com.github.kmu_wink.seoul_in_culture.domain.user.dto;
 
 import com.github.kmu_wink.seoul_in_culture.domain.user.schema.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record UserEditRequest(
+        @Schema(description = "프로필 이미지")
         String avatar,
+
+        @Schema(description = "닉네임")
+
         String nickname,
+
+        @Schema(description = "지역구")
+
         User.District district,
+
+        @Schema(description = "참여 중인 모임 공개 여부")
+
         boolean meetingOpen
 ) {}
