c35d09d3b1fd028e7e2a8b08d1076f0aeab44e7b
didskfo <didskfo2003@gmail.com>
2025-04-05 00:28:49 +0900

    chore(user): 변수 타입 수정

--- src/main/java/com/github/kmu_wink/seoul_in_culture/domain/user/dto/BookmarkDto.java
+++ src/main/java/com/github/kmu_wink/seoul_in_culture/domain/user/dto/BookmarkDto.java
@@ -3,13 +3,15 @@ package com.github.kmu_wink.seoul_in_culture.domain.user.dto;
 import com.github.kmu_wink.seoul_in_culture.domain.event.schema.Event;
 import lombok.Builder;
 
+import java.time.LocalDateTime;
+
 @Builder
 public record BookmarkDto(
         String id,
         Event.Category category,
         String image,
         String title,
-        String startDate,
-        String endDate
+        LocalDateTime startDate,
+        LocalDateTime endDate
 ) {
 }
