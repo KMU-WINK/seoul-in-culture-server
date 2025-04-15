e3ff3e93e26de4241478c98dc124c8ba4d2fdfb8
Son Daehyeon <sondaehyeon01@gmail.com>
2025-04-15 20:36:01 +0900

    refactor(review): 리뷰 스코어 제한(0~5점)

--- src/main/java/com/github/kmu_wink/seoul_in_culture/domain/review/dto/request/CreateReviewRequest.java
+++ src/main/java/com/github/kmu_wink/seoul_in_culture/domain/review/dto/request/CreateReviewRequest.java
@@ -1,8 +1,16 @@
 package com.github.kmu_wink.seoul_in_culture.domain.review.dto.request;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
 public record CreateReviewRequest(
 
+        @Min(0)
+        @Max(5)
         int score,
+
+        @Nullable
         String content
 ) {
 }
